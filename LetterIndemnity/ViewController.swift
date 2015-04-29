4f1c81b3005704a38acfa4651544e170434e6eba
suchartj <cisdsjat@th.ibm.com>
2015-04-29 21:09:37 +0700

    Initial Commit

---
+++ LetterIndemnity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LetterIndemnity
+//
+//  Created by Suchart Jaturasathienchai on 4/29/2558 BE.
+//  Copyright (c) 2558 Suchart Jaturasathienchai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
